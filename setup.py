4972f18f592d521493a6744922efa96f72703aa5
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-11-02 22:58:06 +0000

    Initial code ported over from spectral-cube

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+import numpy
+
+from setuptools import setup
+from setuptools.extension import Extension
+
+setup(use_scm_version={'write_to': os.path.join('casa_formats_io', 'version.py')},
+      ext_modules=[Extension("casa_formats_io._casa_chunking",
+                             [os.path.join('casa_formats_io', '_casa_chunking.c')],
+                             include_dirs=[numpy.get_include()])])
