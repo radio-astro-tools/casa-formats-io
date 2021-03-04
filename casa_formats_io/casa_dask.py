668d86682caefd1d4dc211d692dab489be1ea0ab
Thomas Robitaille <thomas.robitaille@gmail.com>
2021-03-04 16:39:11 +0000

    Added changelog entry

--- casa_formats_io/casa_dask.py
+++ casa_formats_io/casa_dask.py
@@ -240,7 +240,11 @@ def image_to_dask(imagename, memmap=True, mask=False, target_chunksize=None):
             if finished:
                 break
 
-        chunkshape = [c * o for (c, o) in zip(chunkshape, chunkoversample)]
+    else:
+
+        chunkoversample = (1,) * len(chunkshape)
+
+    chunkshape = [c * o for (c, o) in zip(chunkshape, chunkoversample)]
 
     # Create a wrapper that takes slices and returns the appropriate CASA data
     wrapper = CASAArrayWrapper(img_fn, totalshape, chunkshape,
