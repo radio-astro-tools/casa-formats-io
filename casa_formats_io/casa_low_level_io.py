d2cddd3faf14849bc103f96a7261b4bcf35ee446
Thomas Robitaille <thomas.robitaille@gmail.com>
2021-08-12 12:07:34 +0100

    Implement StManAIPSIO reading

--- casa_formats_io/casa_low_level_io.py
+++ casa_formats_io/casa_low_level_io.py
@@ -184,7 +184,7 @@ def read_as_numpy_array(f, value_type, nelem, shape=None):
     Read the next 'nelem' values as a Numpy array
     """
     if value_type == 'string':
-        array = np.array([read_string(f) for i in range(nelem)], dtype='<U16')
+        array = np.array([read_string(f) for i in range(nelem)])
     elif value_type == 'bool':
         array = np.unpackbits(np.frombuffer(f.read(int(np.ceil(nelem / 8)) * 8), dtype='uint8'), bitorder='little').astype(bool)[:nelem]
     elif value_type in TO_DTYPE:
@@ -406,7 +406,7 @@ class Table(BaseCasaObject):
                 if coldata is not None:
                     tbl[colname] = coldata
             else:
-                pass
+                warnings.warn(f'Skipping column {colname} with data manager {dm.__class__.__name__}')
 
         return tbl
 
@@ -914,6 +914,26 @@ class TiledShapeStMan(TiledStMan):
         # super().read_header(f)
 
 
+
+class StManColumnAipsIO(BaseCasaObject):
+
+    @classmethod
+    def read(cls, f, value_type):
+        self = cls()
+        read_int32(f)
+        version = check_type_and_version(f, 'StManColumnAipsIO', 2)
+        self.nr = read_int32(f)
+        irow = 0
+        self.values = []
+        while irow < self.nr:
+            nr = read_int32(f)
+            nr = read_int32(f)
+            self.values.append(read_as_numpy_array(f, value_type, nr))
+            irow += nr
+        self.values = np.hstack(self.values)
+        return self
+
+
 class StManAipsIO(BaseCasaObject):
 
     @classmethod
@@ -924,7 +944,18 @@ class StManAipsIO(BaseCasaObject):
 
     @with_nbytes_prefix
     def read_header(self, f):
-        check_type_and_version(f, 'StManAipsIO', 2)
+        version = check_type_and_version(f, 'StManAipsIO', 2)
+        if version > 1:
+            self.name = read_string(f)
+        self.seqnr = read_int32(f)
+        self.unique_number = read_int32(f)
+        self.nrow = read_int32(f)
+        self.ncol = read_int32(f)
+        self.value_types = [TYPES[read_int32(f)] for icol in range(self.ncol)]
+        self.columns = [StManColumnAipsIO.read(f, self.value_types[icol]) for icol in range(self.ncol)]
+
+    def read_column(self, filename, seqnr, column, coldesc, colindex_in_dm):
+        return self.columns[colindex_in_dm].values
 
 
 class TiledColumnStMan(TiledStMan):
