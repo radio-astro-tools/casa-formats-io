20c141a6f50bdd9cad2a1a678e3deca24b4605da
Thomas Robitaille <thomas.robitaille@gmail.com>
2021-11-25 15:37:22 +0000

    Fixed testing when glue is not present

--- casa_formats_io/tests/test_glue_factory.py
+++ casa_formats_io/tests/test_glue_factory.py
@@ -1,14 +1,14 @@
 import os
-
-from glue.main import load_plugins
-from glue.core.data_factories import load_data
-
+import pytest
 
 DATA = os.path.join(os.path.dirname(__file__), '..', 'casa_low_level_io',
                     'tests', 'data')
 
 
 def test_simple():
+    pytest.importorskip('glue')
+    from glue.main import load_plugins
+    from glue.core.data_factories import load_data
     load_plugins()
     data = load_data(os.path.join(DATA, 'simple.ms'))
     assert len(data) == 2
